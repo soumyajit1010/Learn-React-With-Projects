a4d653d6b1a4bbb7df0f9ea5c95c84da9c77cc8e
Soumyajit Rout <soumyajitrout81@gmail.com>
2025-10-18 16:31:43 +0530

    counter project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(5)
+
+
+
+  //let counter = 15
+  const addValue = () => {
+    //console.log("clicked", counter);
+    if(counter<20){
+          counter = counter+1
+    }
+    setCounter(counter)
+  }
+
+  const removeValue = () => {
+    if(counter>0){
+      counter = counter-1
+    }
+    
+    setCounter(counter)
+  }
+
+
+  return (
+    <>
+      <h1>Chai aur React</h1>
+      <h2>Counter value: {counter}</h2>
+
+      <button
+      onClick={addValue}
+      >Add value</button>
+      <br />
+      <button
+      onClick={removeValue}
+      >Remove value</button>
+    </>
+  )
+}
+
+export default App
